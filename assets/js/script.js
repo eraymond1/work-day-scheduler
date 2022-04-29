cc1f36c4524a3686a026363ceeb771d201fbe0d2
eraymond1 <ericraymond@rocketmail.com>
2022-04-28 21:46:37 -0400

    added script

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+//import {format, compareAsc} from 'date-fns';
+var format;
+
+
+var dates = [
+    new Date(1995, 6, 2),
+    new Date(1987, 1, 11),
+    new Date(1989, 6, 10),
+  ]
+  //dates.sort(compareAsc);
+
+  console.log(dates);
